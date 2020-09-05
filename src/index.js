6106287eefb90260e4a85e9348d7c88739725dc0
Mallory Eastburn <mallorykaitlin@gmail.com>
2020-09-04 19:27:09 -0500

    Responsive adjustment

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
 });
