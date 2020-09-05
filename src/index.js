b46b590fd668b09b18b7484b0222fd33092fda0d
Mallory Eastburn <mallorykaitlin@gmail.com>
2020-09-04 23:47:20 -0500

    Added JS animation to .myName

--- src/index.js
+++ src/index.js
@@ -13,3 +13,26 @@ hamburger.addEventListener("click", () => {
     });
   });
 });
+const text = document.querySelector(".myName");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = text.querySelectorAll("span")[char];
+  span.classList.add("fly");
+  char++;
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
+}
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
